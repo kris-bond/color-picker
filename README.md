8755ac525a8e93f550f86bcaa2e13dcc6ed1ecb3
kris-bond <21047155+kris-bond@users.noreply.github.com>
2022-12-11 17:03:28 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# color-picker
+
+A Google Chrome extension to store values of colours the user clicks on
---
+++ popup.js
@@ -0,0 +1,14 @@
+// adding a new bookmark row to the popup
+const addNewBookmark = () => {};
+
+const viewBookmarks = () => {};
+
+document.addEventListener("DOMContentLoaded", () => {});
+
+document.getElementById("select-col-btn").addEventListener("click", findColor);
+
+function findColor() {
+    document.body.style.cursor = "crosshair";
+
+    
+}
